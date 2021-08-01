b9fe27b564ca2a9cc62a23fcc2aab45a4abc766f
chenty <709384286@qq.com>
2021-08-01 16:34:47 +0800

    upload model files

---
+++ SelectorModels.py
@@ -0,0 +1,14 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class LogisticRegressionModel(nn.Module):
+    def __init__(self, input_length, output_length):
+        super(LogisticRegressionModel, self).__init__()
+        self.linear = torch.nn.Linear(input_length, output_length)
+        self.sigmoid = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.linear(x)
+        y_hat = self.sigmoid(x)
+        return y_hat

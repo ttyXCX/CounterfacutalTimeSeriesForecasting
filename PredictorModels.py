91bcb520c1b57146dedecfe3a27fd4d84dffdf6d
chenty <709384286@qq.com>
2021-08-03 21:22:11 +0800

    models updated

--- PredictorModels.py
+++ PredictorModels.py
@@ -2,4 +2,28 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+
+class MLP(nn.Module):
+    def __init__(self, input_length, output_length, hidden_size=[256, 128]):
+        super(MLP, self).__init__()
+
+        layers = []
+        for i in range(len(hidden_size)):  # loop hidden dimensions
+            if i == 0:
+                layers.append(nn.Linear(input_length, hidden_size[i]))
+            else:
+                layers.append(nn.Linear(hidden_size[i - 1], hidden_size[i]))
+            layers.append(nn.ReLU())
+        layers.append(nn.Linear(hidden_size[-1], output_length))
+        self.mlp = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.mlp(x)
+
+
 # TODO
+# test function
+if __name__ == "__main__":
+    data = torch.randn((100, 96))
+    mlp = MLP(input_length=96, output_length=12, hidden_size=[64, 32])
+    y_hat = mlp.forward(data)
--- SelectorModels.py
+++ SelectorModels.py
@@ -2,15 +2,24 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+
 class LogisticRegression(nn.Module):
     def __init__(self, input_length, output_length):
         super(LogisticRegression, self).__init__()
-        self.linear = torch.nn.Linear(input_length, output_length)
-        self.sigmoid = torch.nn.Sigmoid()
+        self.linear = nn.Linear(input_length, output_length)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = self.linear(x)
-        y_hat = self.sigmoid(x)
-        return y_hat
-    
+        x_out = self.linear(x)
+        x_out = self.sigmoid(x_out)
+        return x_out
+
+
 # TODO
+# test function
+if __name__ == "__main__":
+    data = torch.randn((100, 96))
+    threshold = 0.5  # probability threshold
+    logistic = LogisticRegression(input_length=96, output_length=12)
+    y_hat = logistic.forward(data)
+    y_se = (y_hat >= threshold) + 0
